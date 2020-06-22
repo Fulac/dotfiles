44f8ab31a72b0d4abd93af64610ad9e50fef4a5c
Fulac <full3908@gmail.com>
2020-06-23 00:55:24 +0900

    add cd-ls command

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -27,3 +27,50 @@ function la() {
 }
 
 
+# ------------------------------------------------
+#  cd実行後に自動でls & ショートカット先へ移動
+# ------------------------------------------------
+# cdの後に自動でls
+function cd-ls {
+  param($path)
+
+  try {
+    Set-Location $path -ErrorAction Stop
+    Get-ChildItem
+  }
+  catch {
+    "$_"
+  }
+}
+
+# ショートカットの情報を得る
+filter Get-ShortcutProperty() {
+  $sh  = new-object -comobject WScript.Shell
+  return $sh.CreateShortcut($_)
+}
+
+# ショートカット先へも移動する
+function Move-Location {
+  param($path)
+
+  If( !$path ) { $path = $HOME }
+
+  If( ((Resolve-Path $path).Path).EndsWith(".lnk") ) {
+    $ShortcutPath = Get-ChildItem $path | Get-ShortcutProperty
+
+    If ($ShortcutPath.WorkingDirectory) {
+      cd-ls $ShortcutPath.WorkingDirectory
+    }
+    else {
+      cd-ls $ShortcutPath.TargetPath
+    }
+  }
+  else {
+    cd-ls $path
+  }
+}
+
+# エイリアス設定
+Remove-Item alias:cd
+Set-Alias cd Move-Location
+
