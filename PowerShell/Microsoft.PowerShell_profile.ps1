aaf1bd7fde79fd5ec02152e73f6595cdb8349cc4
Fulac <full3908@gmail.com>
2020-06-22 23:21:14 +0900

    make PowerShell directory

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,30 @@
+# ------------------------------------------------
+#  Prompt設定
+# ------------------------------------------------
+function global:prompt {
+  # プロンプトの設定
+  $host.UI.Write( "Cyan", $host.UI.RawUI.BackgroundColor, "$env:USERNAME " );
+  $host.UI.WriteLine( "Green", $host.UI.RawUI.BackgroundColor, "[" + (Get-Location).Path + "]" );
+  "> " 
+}
+
+
+# ------------------------------------------------
+#  PSReadLineを使った設定
+# ------------------------------------------------
+# Ctrl+dでPowerShellを終了する
+Set-PSReadLineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
+
+
+# ------------------------------------------------
+#  Alias設定
+# ------------------------------------------------
+# 単純なコマンドのエイリアス
+Set-Alias clr clear
+
+# 複雑なコマンドのエイリアス(ex. オプション付き 等)
+function la() {
+  ls -Hidden
+}
+
+
