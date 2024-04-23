39ec330e2ce1144e20a643fc796ac4cf00668bf0
full3908@gmail.com <fulac@arch-corei9.localdomain>
2024-04-23 22:11:12 +0900

    バグ修正(args変数定義とif文の']'に空白挿入)

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ $# -ge 2 ]; then
   exit 1
 fi
 
-args = ${1:-client}
+args=${1:-client}
 
 ### fish
 FISH_CONFIG_PATH="${HOME}/.config/fish"
@@ -19,11 +19,11 @@ if [ -d ${fish_conf_path} ]; then
   ln -sf ~/dotfiles/fish/conf.d/ls_after_cd.fish ~/.config/fish/conf.d/ls_after_cd.fish
   echo "make symbolic link 'fish/conf.d/ls_after_cd.fish' at ~/.config/fish/conf.d/"
 
-  if [ $args = "-client"] || [ $args = "-c" ]; then
+  if [ $args = "-client" ] || [ $args = "-c" ]; then
     # fish_prompt.fish
     ln -sf ~/dotfiles/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
     echo "make symbolic link 'fish/functions/fish_prompt.fish' at ~/.config/fish/functions/"
-  elif [ $args = "-server"] || [ $args = "-s" ]; then 
+  elif [ $args = "-server" ] || [ $args = "-s" ]; then 
     # fish_prompt.fish
     ln -sf ~/dotfiles/fish/functions/fish_server_prompt.fish ~/.config/fish/functions/fish_prompt.fish
     echo "make symbolic link 'fish/functions/fish_server_prompt.fish' at ~/.config/fish/functions/"
