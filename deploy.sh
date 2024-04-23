74559d2da8232aec3760dadefde5a6b26eb5ae48
full3908@gmail.com <fulac@arch-corei9.localdomain>
2024-04-23 22:05:50 +0900

    デスクトップ用、サーバ用でfishのプロンプトを変更する機能追加

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ $# -ge 2 ]; then
+  echo "Usage: $0 -client(-c) or -server(-s) (Default: -client)"
+  exit 1
+fi
+
+args = ${1:-client}
+
 ### fish
 FISH_CONFIG_PATH="${HOME}/.config/fish"
 
@@ -12,9 +19,15 @@ if [ -d ${fish_conf_path} ]; then
   ln -sf ~/dotfiles/fish/conf.d/ls_after_cd.fish ~/.config/fish/conf.d/ls_after_cd.fish
   echo "make symbolic link 'fish/conf.d/ls_after_cd.fish' at ~/.config/fish/conf.d/"
 
-  # fish_prompt.fish
-  ln -sf ~/dotfiles/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
-  echo "make symbolic link 'fish/functions/fish_prompt.fish' at ~/.config/fish/functions/"
+  if [ $args = "-client"] || [ $args = "-c" ]; then
+    # fish_prompt.fish
+    ln -sf ~/dotfiles/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+    echo "make symbolic link 'fish/functions/fish_prompt.fish' at ~/.config/fish/functions/"
+  elif [ $args = "-server"] || [ $args = "-s" ]; then 
+    # fish_prompt.fish
+    ln -sf ~/dotfiles/fish/functions/fish_server_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+    echo "make symbolic link 'fish/functions/fish_server_prompt.fish' at ~/.config/fish/functions/"
+  fi
 fi
 
 ### Neovim
---
+++ fish/functions/fish_server_prompt.fish
@@ -0,0 +1,4 @@
+function fish_prompt
+  fishline -s $status USERHOST SIGSTATUS JOBS GIT WRITE FULLPWD SEPARATOR N ROOT
+end
+
