baba6666ea35fb0c608e704cfd5b15b6069a2e23
fulac <full3908@gmail.com>
2024-12-12 00:22:55 +0900

    nvim-treesitterにrobotframeworkを追加

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return{
     local config = require('nvim-treesitter.configs')
 
     config.setup({
-      ensure_installed = {'c', 'cpp', 'lua', 'vim', 'vimdoc', 'python', 'ssh_config','javascript', 'html', 'json', 'yaml', 'tcl'},
+      ensure_installed = {'c', 'cpp', 'lua', 'vim', 'vimdoc', 'python', 'robot', 'ssh_config','javascript', 'html', 'json', 'yaml', 'tcl'},
       highlight = {enable = true},
       indent = {enable = true},
     })
