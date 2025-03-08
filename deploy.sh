9bd1ddb8fbd0fbfaa5512934cd3bebd3ad0636d4
fulac <full3908@gmail.com>
2025-03-09 03:29:48 +0900

    deploy.shにエラー処理を追加

--- deploy.sh
+++ deploy.sh
@@ -7,35 +7,67 @@ fi
 
 args=${1:-client}
 
-### fish
+### Variables
+DOTFILES_PATH=$(cd $(dirname $0); pwd)
+DOTFILES_FISH_PATH="${DOTFILES_PATH}/fish"
+DOTFILES_VIM_PATH="${DOTFILES_PATH}/vim"
+DOTFILES_NVIM_PATH="${DOTFILES_PATH}/nvim"
 FISH_CONFIG_PATH="${HOME}/.config/fish"
+VIM_CONFIG_PATH="${HOME}/.vim"
+NVIM_CONFIG_PATH="${HOME}/.config/nvim"
 
-if [ -d ${fish_conf_path} ]; then
+### fish
+if [ -d ${FISH_CONFIG_PATH} ]; then
   # config.fish
-  ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
-  echo "make symbolic link 'fish/config.fish' at ~/.config/fish/"
+  if [ -e ${FISH_CONFIG_PATH}/config.fish ]; then
+    unlink ${FISH_CONFIG_PATH}/config.fish
+  fi
+  ln -sf ${DOTFILES_FISH_PATH}/config.fish ${FISH_CONFIG_PATH}/config.fish
+  echo "make symbolic link 'fish/config.fish' at ${FISH_CONFIG_PATH}"
 
   # ls_after_cd.fish
-  ln -sf ~/dotfiles/fish/conf.d/ls_after_cd.fish ~/.config/fish/conf.d/ls_after_cd.fish
-  echo "make symbolic link 'fish/conf.d/ls_after_cd.fish' at ~/.config/fish/conf.d/"
+  if [ -e ${FISH_CONFIG_PATH}/conf.d/ls_after_cd.fish ]; then
+    unlink ${FISH_CONFIG_PATH}/conf.d/ls_after_cd.fish
+  fi
+  ln -sf ~/dotfiles/fish/conf.d/ls_after_cd.fish ${FISH_CONFIG_PATH}/conf.d/ls_after_cd.fish
+  echo "make symbolic link 'fish/conf.d/ls_after_cd.fish' at ${FISH_CONFIG_PATH}/fish/conf.d"
 
+  # fish_prompt.fish
+  if [ -e ${FISH_CONFIG_PATH}/functions/fish_prompt.fish ]; then
+    unlink ${FISH_CONFIG_PATH}/functions/fish_prompt.fish
+  fi
   if [ $args = "-client" ] || [ $args = "-c" ]; then
-    # fish_prompt.fish
-    ln -sf ~/dotfiles/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
-    echo "make symbolic link 'fish/functions/fish_prompt.fish' at ~/.config/fish/functions/"
+    ln -sf ~/dotfiles/fish/functions/fish_prompt.fish ${FISH_CONFIG_PATH}/functions/fish_prompt.fish
+    echo "make symbolic link 'fish/functions/fish_prompt.fish' at ${FISH_CONFIG_PATH}/functions"
   elif [ $args = "-server" ] || [ $args = "-s" ]; then 
-    # fish_prompt.fish
-    ln -sf ~/dotfiles/fish/functions/fish_server_prompt.fish ~/.config/fish/functions/fish_prompt.fish
-    echo "make symbolic link 'fish/functions/fish_server_prompt.fish' at ~/.config/fish/functions/"
+    ln -sf ~/dotfiles/fish/functions/fish_server_prompt.fish ${FISH_CONFIG_PATH}/functions/fish_prompt.fish
+    echo "make symbolic link 'fish/functions/fish_server_prompt.fish' at ${FISH_CONFIG_PATH}/functions"
   fi
 fi
 
-### Vim/Neovim
-if [ $args = "-client" ] || [ $args = "-c" ]; then
-  ln -sf ~/dotfiles/nvim ~/.config/nvim
-  echo "make symbolic link 'nvim' at ~/.config/"
-elif [ $args = "-server" ] || [ $args = "-s" ]; then 
-  ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
-  echo "make symbolic link 'vimrc' at ~/"
+### Neovim
+if [ -L ${NVIM_CONFIG_PATH} ]; then
+  unlink ${NVIM_CONFIG_PATH}
+elif [ -d ${NVIM_CONFIG_PATH} ]; then
+  rm -rf ${NVIM_CONFIG_PATH}
 fi
+ln -sf ${DOTFILES_NVIM_PATH} ${NVIM_CONFIG_PATH}
+echo "make symbolic link 'nvim' at ${HOME}/.config"
+
+### Vim
+# .vimrc
+if [ -e ${VIM_CONFIG_PATH} ]; then
+  unlink ${VIM_CONFIG_PATH}
+fi
+ln -sf ${DOTFILES_VIM_PATH} ${VIM_CONFIG_PATH}
+echo "make symbolic link 'vimrc' at ${HOME}"
+
+# .vim
+if [ -L ${VIM_CONFIG_PATH} ]; then
+  unlink ${VIM_CONFIG_PATH}
+elif [ -d ${VIM_CONFIG_PATH} ]; then
+  rm -rf ${VIM_CONFIG_PATH}
+fi
+ln -sf ${DOTFILES_VIM_PATH} ${VIM_CONFIG_PATH}
+echo "make symbolic link 'vim' at ${HOME}"
 
