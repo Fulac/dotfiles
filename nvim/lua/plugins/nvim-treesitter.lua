de8286755e3cb144d1ceb6d94a036e82de8c8400
fulac <full3908@gmail.com>
2026-01-16 12:07:07 +0900

    nvim-treesitterのインストールにlazyの自動セットアップ機能を利用する

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,18 +1,15 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  -- 起動時に読み込むためのイベント
-  event = { "BufReadPost", "BufNewFile" },
+  main = 'nvim-treesitter.configs',
   opts = {
     ensure_installed = { 
       'c', 'cpp', 'lua', 'vim', 'vimdoc', 'python', 
       'robot', 'ssh_config', 'javascript', 'html', 
-      'json', 'yaml', 'tcl', 'query'
+      'json', 'yaml', 'tcl', 'query' 
     },
     highlight = { enable = true },
     indent = { enable = true },
   },
-  config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
-  end,
+  event = { "BufReadPost", "BufNewFile" },
 }
