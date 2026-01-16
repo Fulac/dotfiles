8af60bac20615a3b5fd9f28565230feea8dd548c
fulac <full3908@gmail.com>
2026-01-16 19:15:36 +0900

    起動順序の調整

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,20 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs',
-  opts = {
-    ensure_installed = { 
-      'c', 'cpp', 'lua', 'vim', 'vimdoc', 'python', 
-      'robot', 'ssh_config', 'javascript', 'html', 
-      'json', 'yaml', 'tcl', 'query' 
-    },
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  -- event 指定で、Neovimが完全に起動してパスが通った後に読み込ませる
   event = { "BufReadPost", "BufNewFile" },
+  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+  config = function()
+    -- pcall で保護し、失敗時にメッセージを出す
+    local status, configs = pcall(require, "nvim-treesitter.configs")
+    if not status then
+      return
+    end
+
+    configs.setup({
+      ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "python", "javascript", "html", "json", "yaml" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
